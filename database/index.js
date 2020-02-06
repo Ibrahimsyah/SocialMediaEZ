f2964b043311be06ce7b0a8cd8a4a9b2deff81bc
Muhammad Wildan Aldiansyah <aldiwild77@gmail.com>
2020-02-07 01:48:58 +0700

    test current pool connection

--- database/index.js
+++ database/index.js
@@ -6,7 +6,7 @@ const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD
-})
+});
 
 db.query('select 1 + 1 as result', (err, res)=>{
     if(err) console.log(err)
